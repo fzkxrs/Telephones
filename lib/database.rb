1b447186d23c0d0cb97bdcf750589bf814567f49
Artem <wolflammen0@gmail.com>
2024-09-19 13:33:49 +0300

    Fix db

--- lib/database.rb
+++ lib/database.rb
@@ -1,31 +1,56 @@
 require 'pg'
 
 class Database
-  @connection = nil
   def initialize(db_config)
     @adapter = db_config['adapter']
     @dbname = db_config['database']
     @user = db_config['username']
     @password = db_config['password']
     @host = db_config['host']
-    @employees_table_name = "data"
+    @data_table = "data"
+    @phones_table = "phones"
   end
 
   public
-  def search_employee(fio="null")
-    # Query the database
+  def search_employee(fio, enterprise, department, group, lab, position, corp_inner_tel, inner_tel)
+    # Construct the query to search both tables
     query = <<-SQL
-            SELECT 
-              enterprise, 
-              department 
-            FROM 
-              employees.#{@employees_table_name}
-            WHERE 
-              fio = $1 
-            ORDER BY 
-              enterprise ASC;
-          SQL
-    execute_query(query, fio)
+      SELECT 
+        data.id,
+        data.fio,
+        data.enterprise,
+        data.department,
+        data.group,
+        data.lab,
+        data.position,
+        data.email,
+        data.address,
+        data.corp_inner_tel,
+        data.inner_tel,
+        phones.phone,
+        phones.fax,
+        phones.modem,
+        phones.mg
+      FROM 
+        employees.#{@data_table} AS data
+      LEFT JOIN 
+        employees.#{@phones_table} AS phones
+      ON 
+        data.id = phones.id
+      WHERE
+        (data.fio ILIKE $1 OR $1 IS NULL)
+        AND (data.enterprise ILIKE $2 OR $2 IS NULL)
+        AND (data.department ILIKE $3 OR $3 IS NULL)
+        AND (data.group ILIKE $4 OR $4 IS NULL)
+        AND (data.lab ILIKE $5 OR $5 IS NULL)
+        AND (data.position ILIKE $6 OR $6 IS NULL)
+        AND (data.corp_inner_tel = $7 OR $7 IS NULL)
+        AND (data.inner_tel = $8 OR $8 IS NULL)
+      ORDER BY 
+        data.enterprise ASC;
+    SQL
+
+    execute_query(query, fio, enterprise, department, group, lab, position, corp_inner_tel, inner_tel)
   end
 
   private
@@ -43,6 +68,6 @@ class Database
   end
 
   def close
-    self.close_connection
+    @connection&.close
   end
 end
